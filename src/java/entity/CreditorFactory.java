d554ad8eafd2e1ff526a90165da31d2de3673faa
EdisonLim17 <117683125+EdisonLim17@users.noreply.github.com>
2024-11-09 19:00:07 -0500

    Added the Creditor Entity

---
+++ src/java/entity/CreditorFactory.java
@@ -0,0 +1,14 @@
+package entity;
+
+/**
+ * Factory for creating creditors.
+ */
+public interface CreditorFactory {
+    /**
+     * Creates a new creditor
+     * @param name the name of the creditor
+     * @param amountOwed the amount of money (in Canadian dollars) the user owes the creditor
+     * @return the new creditor
+     */
+    Creditor create(String name, double amountOwed);
+}
